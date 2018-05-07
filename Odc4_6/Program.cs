1e9cbe383676d412d3e83e621bb0ca77b51f163e
jakub.pelka94 <jakub.pelka94@outlook.com>
2018-05-07 21:56:46 +0200

    Program

--- Odc4_6/Program.cs
+++ Odc4_6/Program.cs
@@ -10,6 +10,31 @@ namespace Odc4_6
     {
         static void Main(string[] args)
         {
+            Console.Write ("Podaj swój wiek: ");
+            int age; 
+            
+            bool result = int.TryParse(Console.ReadLine(), out age);
+            if (result == false)
+            {
+                Console.WriteLine("Podaj wiek jeszcze raz: ");
+            }
+
+            else
+            {
+                if (age <= 18)
+                { 
+                    Console.WriteLine("Moze soczek cwelu");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Kurwa już podaje browarka");
+                }
+
+            }
+            Console.ReadKey();
         }
+            
+      }
     }
-}
+
