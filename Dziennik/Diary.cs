01bf9c6a3cc87d45647f36cca425dde04d36da69
jakub.pelka94 <jakub.pelka94@outlook.com>
2018-05-08 17:10:13 +0200

    v.1.1

---
+++ Dziennik/Diary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziennik
+{
+    class Diary
+    {
+        // Stan (zmienne-pola)
+
+        List <float> oceny;
+
+        // Zachowania
+        public void dodawanie (float ocena)
+        {
+            oceny.Add(ocena);            
+        }
+            
+        public float srednia()
+        {
+            float suma=0 , sre=0;
+            foreach (var ocena in  oceny)
+            {
+                suma += ocena;
+            }
+            sre = suma / oceny.Count();
+
+            return sre;
+        }
+
+        public float maxocena()
+        {
+            return oceny.Max();
+        }
+
+        public float minocena()
+        {
+            return oceny.Min();
+        }
+
+
+    }
+}
